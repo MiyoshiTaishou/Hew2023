92fbe5608b4cf20f65c42c330448b129958748ca
MiyoshiTaishou <99778743+MiyoshiTaishou@users.noreply.github.com>
2023-09-22 09:26:06 +0900

    Octopus
    タコオブジェ実装

---
+++ Hew_Miyoshi_2023/Octopus.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "gameObject.h"
+#include"animationModel.h"
+
+class Octopus :
+    public GameObject
+{
+public:
+    void Init() override;   
+private:
+    //class AnimationModel* m_Model;
+};
+
