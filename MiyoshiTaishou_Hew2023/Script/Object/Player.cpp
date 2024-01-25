61280c8e962e2071a0ea431c383557039ff9a197
MiyoshiTaishou <99778743+MiyoshiTaishou@users.noreply.github.com>
2024-01-25 10:48:19 +0900

    RotPlayer
    プレイヤーの周りの点の処理を最適化

--- MiyoshiTaishou_Hew2023/Script/Object/Player.cpp
+++ MiyoshiTaishou_Hew2023/Script/Object/Player.cpp
@@ -194,82 +194,8 @@ void Player::Update()
 
 	//GetComponent<RigidBody>()->AddTorque(torque, ForceMode::Force);
 
-	//左端
-	m_Point[0] = Vector3(m_Position.x - (m_Scale.x / 2), m_Position.y, m_Position.z);
-	//右端
-	m_Point[1] = Vector3(m_Position.x + (m_Scale.x / 2), m_Position.y, m_Position.z);
-	//下
-	m_Point[2] = Vector3(m_Position.x, m_Position.y - (m_Scale.y / 2), m_Position.z);
-	//上
-	m_Point[3] = Vector3(m_Position.x, m_Position.y + (m_Scale.y / 2), m_Position.z);
-	//手前
-	m_Point[4] = Vector3(m_Position.x, m_Position.y, m_Position.z - (m_Scale.z / 2));
-	//奥
-	m_Point[5] = Vector3(m_Position.x, m_Position.y, m_Position.z + (m_Scale.z / 2));	
-	//左下
-	m_Point[6] = (m_Point[0] + m_Point[2]) / 2;
-	m_Point[6].x -= m_Distance[6];
-	m_Point[6].y -= m_Distance[6];
-	//左上
-	m_Point[7] = (m_Point[0] + m_Point[3]) / 2;
-	m_Point[7].x -= m_Distance[7];
-	m_Point[7].y += m_Distance[7];
-	//左手前
-	m_Point[8] = (m_Point[0] + m_Point[4]) / 2;
-	m_Point[8].x -= m_Distance[8];
-	m_Point[8].z -= m_Distance[8];
-	//左奥
-	m_Point[9] = (m_Point[0] + m_Point[5]) / 2;
-	m_Point[9].x -= m_Distance[9];
-	m_Point[9].z += m_Distance[9];
-	//右下
-	m_Point[10] = (m_Point[1] + m_Point[2]) / 2;
-	m_Point[10].x += m_Distance[10];
-	m_Point[10].y -= m_Distance[10];
-	//右上
-	m_Point[11] = (m_Point[1] + m_Point[3]) / 2;
-	m_Point[11].x += m_Distance[11];
-	m_Point[11].y += m_Distance[11];
-	//右手前
-	m_Point[12] = (m_Point[1] + m_Point[4]) / 2;
-	m_Point[12].x += m_Distance[12];
-	m_Point[12].z -= m_Distance[12];
-	//右奥
-	m_Point[13] = (m_Point[1] + m_Point[5]) / 2;
-	m_Point[13].x += m_Distance[13];
-	m_Point[13].z += m_Distance[13];
-	//左端
-	m_Point[0] = Vector3(m_Position.x - (m_Scale.x / 2) - m_Distance[0], m_Position.y, m_Position.z);
-	//右端
-	m_Point[1] = Vector3(m_Position.x + (m_Scale.x / 2) + m_Distance[1], m_Position.y, m_Position.z);
-	//下
-	m_Point[2] = Vector3(m_Position.x, m_Position.y - (m_Scale.y / 2) - m_Distance[2], m_Position.z);
-	//上
-	m_Point[3] = Vector3(m_Position.x, m_Position.y + (m_Scale.y / 2) + m_Distance[3], m_Position.z);
-	//手前
-	m_Point[4] = Vector3(m_Position.x, m_Position.y, m_Position.z - (m_Scale.z / 2) - m_Distance[4]);
-	//奥
-	m_Point[5] = Vector3(m_Position.x, m_Position.y, m_Position.z + (m_Scale.z / 2) + m_Distance[5]);
-
-
-	// ポイントをプレイヤーの回転に合わせて変換する
-	for (int i = 0; i < MAX_SPHERE_MESH; ++i) {
-		// ポイントをベクトルに変換
-		Vector3 point = m_Point[i];
-
-		point -= m_Position;
-
-		// ポイントを行列で回転させる
-		Matrix rotationMatrix = m_Rotmatrix;
-		Vector3 rotatedVector;
-		rotatedVector.x = rotationMatrix._11 * point.x + rotationMatrix._12 * point.y + rotationMatrix._13 * point.z;
-		rotatedVector.y = rotationMatrix._21 * point.x + rotationMatrix._22 * point.y + rotationMatrix._23 * point.z;
-		rotatedVector.z = rotationMatrix._31 * point.x + rotationMatrix._32 * point.y + rotationMatrix._33 * point.z;
-		
-		rotatedVector += m_Position;
-		// 回転後のポイントを更新
-		m_Point[i] = rotatedVector;
-	}
+	//座標計算
+	PointUpdate();
 
 	//パーティクル更新処理
 	m_Particle->Update();
@@ -701,4 +627,80 @@ void Player::ConInput()
 
 	this->accumulatedRotation *= Quaternion::CreateFromYawPitchRoll(yaw, pitch, roll);
 	m_Rotmatrix = Matrix::CreateFromQuaternion(accumulatedRotation);
+}
+
+void Player::PointUpdate()
+{
+	//左端
+	m_Point[0] = Vector3(m_Position.x - (m_Scale.x / 2), m_Position.y, m_Position.z);
+	//右端
+	m_Point[1] = Vector3(m_Position.x + (m_Scale.x / 2), m_Position.y, m_Position.z);
+	//下
+	m_Point[2] = Vector3(m_Position.x, m_Position.y - (m_Scale.y / 2), m_Position.z);
+	//上
+	m_Point[3] = Vector3(m_Position.x, m_Position.y + (m_Scale.y / 2), m_Position.z);
+	//手前
+	m_Point[4] = Vector3(m_Position.x, m_Position.y, m_Position.z - (m_Scale.z / 2));
+	//奥
+	m_Point[5] = Vector3(m_Position.x, m_Position.y, m_Position.z + (m_Scale.z / 2));
+	//左下
+	m_Point[6] = (m_Point[0] + m_Point[2]) / 2;
+	m_Point[6].x -= m_Distance[6];
+	m_Point[6].y -= m_Distance[6];
+	//左上
+	m_Point[7] = (m_Point[0] + m_Point[3]) / 2;
+	m_Point[7].x -= m_Distance[7];
+	m_Point[7].y += m_Distance[7];
+	//左手前
+	m_Point[8] = (m_Point[0] + m_Point[4]) / 2;
+	m_Point[8].x -= m_Distance[8];
+	m_Point[8].z -= m_Distance[8];
+	//左奥
+	m_Point[9] = (m_Point[0] + m_Point[5]) / 2;
+	m_Point[9].x -= m_Distance[9];
+	m_Point[9].z += m_Distance[9];
+	//右下
+	m_Point[10] = (m_Point[1] + m_Point[2]) / 2;
+	m_Point[10].x += m_Distance[10];
+	m_Point[10].y -= m_Distance[10];
+	//右上
+	m_Point[11] = (m_Point[1] + m_Point[3]) / 2;
+	m_Point[11].x += m_Distance[11];
+	m_Point[11].y += m_Distance[11];
+	//右手前
+	m_Point[12] = (m_Point[1] + m_Point[4]) / 2;
+	m_Point[12].x += m_Distance[12];
+	m_Point[12].z -= m_Distance[12];
+	//右奥
+	m_Point[13] = (m_Point[1] + m_Point[5]) / 2;
+	m_Point[13].x += m_Distance[13];
+	m_Point[13].z += m_Distance[13];
+	//左端
+	m_Point[0] = Vector3(m_Position.x - (m_Scale.x / 2) - m_Distance[0], m_Position.y, m_Position.z);
+	//右端
+	m_Point[1] = Vector3(m_Position.x + (m_Scale.x / 2) + m_Distance[1], m_Position.y, m_Position.z);
+	//下
+	m_Point[2] = Vector3(m_Position.x, m_Position.y - (m_Scale.y / 2) - m_Distance[2], m_Position.z);
+	//上
+	m_Point[3] = Vector3(m_Position.x, m_Position.y + (m_Scale.y / 2) + m_Distance[3], m_Position.z);
+	//手前
+	m_Point[4] = Vector3(m_Position.x, m_Position.y, m_Position.z - (m_Scale.z / 2) - m_Distance[4]);
+	//奥
+	m_Point[5] = Vector3(m_Position.x, m_Position.y, m_Position.z + (m_Scale.z / 2) + m_Distance[5]);
+
+
+	// ポイントをプレイヤーの回転に合わせて変換する
+	for (int i = 0; i < MAX_SPHERE_MESH; ++i)
+	{		
+
+		// ポイントをベクトルに変換
+		Vector3 point = m_Point[i] - m_Position;
+
+		// 行列で回転させる
+		Matrix rotationMatrix = m_Rotmatrix;
+		Vector3 rotatedVector = Vector3::Transform(point, rotationMatrix);
+
+		// 回転後のポイントを更新
+		m_Point[i] = rotatedVector + m_Position;
+	}
 }
